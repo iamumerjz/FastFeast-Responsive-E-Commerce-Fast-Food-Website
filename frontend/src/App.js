a45942dcf1a994436db52bf5e5a17341d10d0e02
Muhammad Umer Ijaz <160628747+iamumerjz@users.noreply.github.com>
2026-03-17 15:38:48 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ import "./App.css";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-const STRIPE_PUBLISHABLE_KEY = "your_stripe_key";
+const STRIPE_PUBLISHABLE_KEY = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;;
 const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
 
 function App() {
